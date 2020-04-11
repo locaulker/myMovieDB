bce5d6e7db25bbf635fa769af58f4974f21955ab
Lawrence <Caulker>
2020-04-11 00:17:29 -0400

    refactored class component to functional component

--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,16 @@
-import React, { Component } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 
-export default class Movie extends Component {
-  static propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired
-    }),
-    desc: PropTypes.string
-  }
+const Movie = ({ movie }) => (
+  <div>
+    <h3>{movie.title}</h3>
+  </div>
+)
 
-  static defaultProps = {
-    desc: "Description is not available"
-  }
+export default Movie
 
-  render() {
-    return (
-      <div>
-        <h3>{this.props.movie.title}</h3>
-      </div>
-    )
-  }
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired
+  })
 }
