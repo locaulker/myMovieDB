13dcbe4d568dfc995c775677bc1d9ca423e93fa7
Lawrence <Caulker>
2020-04-11 17:37:51 -0400

    installed and applied styled-components

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import { Poster } from "./Movie"
+import styled from "styled-components"
 
-const POSTER_PATH = 'http://image.tmdb.org/t/p/w154'
-const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280'
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154"
+const BACKDROP_PATH = "http://image.tmdb.org/t/p/w1280"
 
 class MovieDetail extends Component {
 	state = {
@@ -9,8 +11,7 @@ class MovieDetail extends Component {
 	}
 
 	async componentDidMount() {
-		const url = `https://api.themoviedb.org/3/movie/${this.props.match.params
-			.id}?api_key=c1104b67d4a4c77cc42b34a4c561c8f3&language=en-US`
+		const url = `https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=c1104b67d4a4c77cc42b34a4c561c8f3&language=en-US`
 		try {
 			const res = await fetch(url)
 			const movie = await res.json()
@@ -26,15 +27,41 @@ class MovieDetail extends Component {
 	render() {
 		const { movie } = this.state
 		return (
-			<div>
-				<img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-				<img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-				<h1>{movie.title}</h1>
-				<h3>{movie.release_date}</h3>
-				<p>{movie.overview}</p>
-			</div>
+			<MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+				<MovieInfo>
+					<Poster
+						src={`${POSTER_PATH}${movie.poster_path}`}
+						alt={movie.title}
+					/>
+					<div>
+						<h1>{movie.title}</h1>
+						<h3>{movie.release_date}</h3>
+						<p>{movie.overview}</p>
+					</div>
+				</MovieInfo>
+			</MovieWrapper>
 		)
 	}
 }
 
 export default MovieDetail
+
+const MovieWrapper = styled.div`
+	position: relative;
+	padding-top: 50vh;
+	background: url(${props => props.backdrop}) no-repeat;
+	background-size: cover;
+`
+const MovieInfo = styled.div`
+	background: white;
+	text-align: left;
+	padding: 2rem 10%;
+	display: flex;
+	> div {
+		margin-left: 20px;
+	}
+	img {
+		position: relative;
+		top: -5rem;
+	}
+`
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,36 +1,44 @@
 import React, { Component } from "react"
 import Movie from "./Movie"
-
+import styled from "styled-components"
 
 class MoviesList extends Component {
-  state = {
-    movies: []
-  }
+	state = {
+		movies: []
+	}
 
-  async componentDidMount() {
-    const url =
-      "https://api.themoviedb.org/3/discover/movie?api_key=c1104b67d4a4c77cc42b34a4c561c8f3&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
-    try {
-      const res = await fetch(url)
-      const movies = await res.json()
+	async componentDidMount() {
+		const url =
+			"https://api.themoviedb.org/3/discover/movie?api_key=c1104b67d4a4c77cc42b34a4c561c8f3&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+		try {
+			const res = await fetch(url)
+			const movies = await res.json()
 
-      this.setState({
-        movies: movies.results
-      })
-    } catch (e) {
-      console.log(e)
-    }
-  }
+			this.setState({
+				movies: movies.results
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  render() {
-    return (
-        <div>
-        {this.state.movies.map(movie => (
-            <Movie key={movie.id} movie={movie} />
-          ))}
-        </div>
-    )
-  }
+	render() {
+		return (
+			<MovieGrid>
+				{this.state.movies.map(movie => (
+					<Movie key={movie.id} movie={movie} />
+				))}
+			</MovieGrid>
+		)
+	}
 }
-
 export default MoviesList
+
+// Styled Component
+const MovieGrid = styled.div`
+	display: grid;
+	padding: 1rem;
+	grid-template-columns: repeat(6, 1fr);
+	grid-row-gap: 1rem;
+	grid-column-gap: 1rem;
+`
