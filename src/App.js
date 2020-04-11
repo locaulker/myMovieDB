769ecfb63f1c36cb1d48083a1a26c12021654650
Lawrence <Caulker>
2020-04-10 22:00:30 -0400

    signed up and used api to retrieved movie data

--- src/App.js
+++ src/App.js
@@ -3,35 +3,36 @@ import logo from "./logo.svg"
 import "./App.css"
 import Movie from "./Movie"
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars'
-  },
-  {
-    id: 2,
-    title: 'Spider Man'
-  },
-  {
-    id: 3,
-    title: '36th Chamber of Shaolin'
-  },
-  {
-    id: 4,
-    title: '5 Deadly Venoms'
-  },
-]
-
-
 class App extends Component {
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    const url =
+      "https://api.themoviedb.org/3/discover/movie?api_key=c1104b67d4a4c77cc42b34a4c561c8f3&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+    try {
+      const res = await fetch(url)
+      const movies = await res.json()
+
+      this.setState({
+        movies: movies.results
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   render() {
     return (
+      // console.log(this.state.movies),
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     )
   }
